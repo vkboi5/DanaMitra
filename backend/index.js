bc477390ca0771b6b15a9286af889f5e48f70e13
vkboi5 <vkirthikjan5@gmail.com>
2025-02-22 17:03:54 +0530

    changes done

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong!" });
 });
 
+app.get("/",(req,res)=>{
+  res.send("Server is healthy")
+})
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
